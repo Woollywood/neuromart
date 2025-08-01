a1d8aec934e63c1c80acc494446a286f5ce3b84a
a.cherkashin <a.cherkashin@notamedia.com.ru>
2025-08-01 15:37:04 +0300

    feat: промежуточный результат адаптива главной страницы

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+const concatString = (/** @type {string[]} */ strs) => `${strs.join(';')};`;
+
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: concatString([`@use '@/styles/variables.scss' as *;`, `@use '@/styles/mixins.scss' as *;`]),
+        },
+      },
+    },
+  },
   integrations: [icon()],
 });
